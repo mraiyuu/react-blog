efa9415237cabbf0bbd662b8b245c263586a8e7b
mraiyuu <munenedenis618@gmail.com>
2024-01-15 07:42:04 +0300

    shell taking shape

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
-import './App.css';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-       <Home />
+        <Home />
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useState} from "react";
 
 const Home = () => {
+
+    const [name, setName] = useState('Denis')
+    const [age, setAge] = useState(30)
+
+    const handleClick = () => {
+        setName('Jack')
+        setAge(23)
+    }
+
   return (
     <div className="home">
-        <h2>
-            Homepage
-        </h2>
+      <h2>Homepage</h2>
+      <p>{name} is {age}</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
