d3116fa7660d64e5e692c38b45b270b4bafc9c53
mraiyuu <munenedenis618@gmail.com>
2024-01-15 06:54:04 +0300

    shell

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <nav className="navbar">
+    <h1>
+        The Dojo blog
+    </h1>
+    <div className="links">
+        <a href='/'>Home</a>
+        <a href='/create'>New blog</a>
+    </div>
+   </nav>
+  )
+}
+
+export default Navbar
