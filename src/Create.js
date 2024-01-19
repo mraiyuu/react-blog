231b2ebdeeb1212059464a28b9efd2d99fca831f
mraiyuu <munenedenis618@gmail.com>
2024-01-19 06:11:31 +0300

    submit event refactor

--- src/Create.js
+++ src/Create.js
@@ -2,28 +2,46 @@ import { useState } from "react";
 
 const Create = () => {
 
-  const [title, setTitle] = useState('type here')
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('yoshi')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const blog = {title, body, author}
+    console.log(blog)
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form
+      onSubmit={handleSubmit}
+      >
         <label>Blog title: </label>
         <input type="text" 
         required
         value={title}
+        onChange={(e) => setTitle(e.target.value)}
          />
 
         <label>Blog body: </label>
         <textarea
         required
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author: </label>
-        <select>
+        <select
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        >
           <option value='mario'>mario</option>
           <option value='yoshi'>yoshi</option>
         </select>
         <button>Add blog</button>
+        <p>{ title }</p>
+        <p>{body}</p>
       </form>
     </div>
   );
