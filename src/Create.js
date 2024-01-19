3b23f4b046ff0a54909fb0ceef1ea04fcab6d996
mraiyuu <munenedenis618@gmail.com>
2024-01-19 05:54:42 +0300

    new blog form

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
+
 const Create = () => {
+
+  const [title, setTitle] = useState('type here')
+
   return (
     <div className="create">
-      <h2>Add a New Blogs</h2>
+      <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title: </label>
+        <input type="text" 
+        required
+        value={title}
+         />
+
+        <label>Blog body: </label>
+        <textarea
+        required
+        ></textarea>
+        <label>Blog author: </label>
+        <select>
+          <option value='mario'>mario</option>
+          <option value='yoshi'>yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
-}
- 
+};
+
 export default Create;
